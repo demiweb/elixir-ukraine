64f0784071f3ae571f24de8fdc1ceb5e5aa4abb3
JukaChu <olexiy.yankevich@gmail.com>
2020-11-26 15:20:04 +0200

    catalog page, header sticky scroll down

--- src/scripts/headerScript.js
+++ src/scripts/headerScript.js
@@ -3,3 +3,22 @@ let burgerBtn = [...document.querySelectorAll('.header-navigation__list-containe
 function toggleVisible() {
     burgerBtn[0].classList.toggle('header-navigation__list-container--active');
 }
+
+let hdr = [...document.querySelectorAll('.elixir-header')];
+
+window.addEventListener('scroll', () => {
+    if (window.innerWidth > 767) {
+
+
+        if (this.oldScroll > this.scrollY) {
+            hdr[0].style.top = '0px';
+        } else {
+            hdr[0].style.top = '-77px';
+
+        }
+        this.oldScroll = this.scrollY;
+    } else {
+        hdr[0].style.top = '0';
+
+    }
+});
